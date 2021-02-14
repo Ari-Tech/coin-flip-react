e33c25c2cd6fc45912cff10d75a8e4600a674b57
Arindam <Lahiri.ari@outlook.com>
2021-02-14 23:47:29 +0800

    coin flip with animation

--- src/CoinFlip.tsx
+++ src/CoinFlip.tsx
@@ -1,10 +1,18 @@
 import React, { useState } from 'react';
 const CoinFlip = (props: any)=>{
     const [tossResult, SetTossResult] = useState(true);
+    const [animate, setAnimate] = useState(false);
+    const flipCoin = ()=>{
+      setAnimate(true)
+      setTimeout(()=>{
+        setAnimate(false)
+        SetTossResult(Math.random()<0.5)
+      }, 2000)
+    }
   return (
   <>
   <div>
-    <div className={"coin"} onClick={()=> SetTossResult(Math.random()<0.5)}>{tossResult?"HEADS":"TAILS"}</div>
+    <div className={animate?"coin-animate":"coin"} onClick={()=> flipCoin()}>{tossResult?"HEADS":"TAILS"}</div>
   </div>
   </>)
 }
